1579e6833447c18b483466d3b7104aca5eb69c2c
AyukGiress <giress865@gmail.com>
2024-02-12 13:23:40 +0100

    editing mistak

--- asset/styles/dom.js
+++ asset/styles/dom.js
@@ -1,47 +1,51 @@
 // targets name and age id
-document.getElementById("name-input").addEventListener("input", function () {
-  //then initiate search function which targets my list
-  let searchValue = this.value.toLowerCase();
-  let listItems = document.getElementById("my-list").getElementsByTagName("li");
 
-  //then we iterate over evry list
+document.getElementById('name-input').addEventListener('input', function () {
+
+  // then initiate search function which targets my list
+  let searchValue = this.value.toLowerCase()
+  let listItems = document.getElementById('my-list').getElementsByTagName('li')
+
+  // then we iterate over evry list
   for (let i = 0; i < listItems.length; i++) {
-    let text = listItems[i].textContent.toLowerCase();
+    let text = listItems[i].textContent.toLowerCase()
     if (text.includes(searchValue)) {
-      listItems[i].style.display = "block";
+      listItems[i].style.display = 'block'
     } else {
-      listItems[i].style.display = "none";
+      listItems[i].style.display = 'none'
     }
   }
-});
+})
 
-//first call the addtolist function and
-//target the  name, age and list id
-function addToList() {
-  let name = document.getElementById("name-input").value;
-  let age = document.getElementById("age-input").value;
-  let list = document.getElementById("my-list");
+// first call the addtolist function and
+// target the  name, age and list id
+function addToList () {
+  let name = document.getElementById('name-input').value
+  let age = document.getElementById('age-input').value
+  let list = document.getElementById('my-list')
 
-  //after add the dot create element
-  //which then is define with textcontent
-  //to add name and age
-  let listItem = document.createElement("li");
-  listItem.textContent = name + "-" + age;
+  // after add the dot create elementk,
+  // which then is define with textcontent
+  // to add name and age
 
-  //then append any child item that is to be added to the listitem
-  list.appendChild(listItem);
-}
-//first target all class of delete buttons
-let deleteButton = document.getElementsByClassName("delete-button");
+  let listItem = document.createElement('li')
+  listItem.textContent = name + '-' + age
 
-//go through the buttons agian
+  // then append any child item that is to be added to the listitem
+  list.appendChild(listItem)
+} 
+
+// first target all class of delete buttons
+let deleteButton = document.getElementsByClassName('delete-button')
+
+// go through the buttons agian
 
 for (let i = 0; i < deleteButton.length; i++) {
-  //add a click funtion to the buttons
+  // add a click funtion to the buttons
 
-  deleteButton[i].addEventListener("click", function () {
-    //finally add the parent node so when button is clicked, it remove a list
+  deleteButton[i].addEventListener('click', function () {
+    // finally add the parent node so when button is clicked, it remove a list
 
-    this.parentNode.remove();
-  });
+    this.parentNode.remove()
+  })
 }
