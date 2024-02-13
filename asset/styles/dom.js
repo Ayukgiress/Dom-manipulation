52c940e4b75b01afd3d115165d6a0fd960550cb6
AyukGiress <giress865@gmail.com>
2024-02-13 09:26:06 +0100

    editing mistake

--- asset/styles/dom.js
+++ asset/styles/dom.js
@@ -1,14 +1,13 @@
 // targets name and age id
 
 document.getElementById('name-input').addEventListener('input', function () {
-
   // then initiate search function which targets my list
-  let searchValue = this.value.toLowerCase()
-  let listItems = document.getElementById('my-list').getElementsByTagName('li')
+  const searchValue = this.value.toLowerCase()
+  const listItems = document.getElementById('my-list').getElementsByTagName('li')
 
   // then we iterate over evry list
   for (let i = 0; i < listItems.length; i++) {
-    let text = listItems[i].textContent.toLowerCase()
+  const text = listItems[i].textContent.toLowerCase()
     if (text.includes(searchValue)) {
       listItems[i].style.display = 'block'
     } else {
@@ -20,23 +19,25 @@ document.getElementById('name-input').addEventListener('input', function () {
 // first call the addtolist function and
 // target the  name, age and list id
 function addToList () {
-  let name = document.getElementById('name-input').value
-  let age = document.getElementById('age-input').value
-  let list = document.getElementById('my-list')
+  const name = document.getElementById('name-input').value
+  const age = document.getElementById('age-input').value
+  const list = document.getElementById('my-list')
 
   // after add the dot create elementk,
   // which then is define with textcontent
   // to add name and age
 
-  let listItem = document.createElement('li')
+  const listItem = document.createElement('li')
   listItem.textContent = name + '-' + age
 
   // then append any child item that is to be added to the listitem
   list.appendChild(listItem)
 } 
 
+addToList()
+
 // first target all class of delete buttons
-let deleteButton = document.getElementsByClassName('delete-button')
+const deleteButton = document.getElementsByClassName('delete-button')
 
 // go through the buttons agian
 
